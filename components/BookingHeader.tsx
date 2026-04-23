ec1b7a56a676ea3e2dadc485447efbe9d89a94d0
Ben <aceben72@gmail.com>
2026-04-23 13:20:48 +1000

    Logo links to main site in client booking flow

--- components/BookingHeader.tsx
+++ components/BookingHeader.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function BookingHeader() {
   return (
     <header className="bg-[#044e77] py-5 px-6 flex items-center justify-center">
-      <Link href="/" className="flex items-center gap-3">
+      <a
+        href="https://cocoonskinandbeauty.com.au"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center gap-3"
+      >
         <Image
           src="https://mcusercontent.com/644ef8c7fbae49e3b1826dda3/images/1b7a3cb7-18c0-682d-62bf-921900b53c86.png"
           alt="Cocoon Skin & Beauty"
@@ -14,7 +18,7 @@ export default function BookingHeader() {
           priority
           unoptimized
         />
-      </Link>
+      </a>
     </header>
   );
 }
